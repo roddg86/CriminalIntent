fe04537d69ba8d42d7ad24384ddd6462ceb61dc1
roddg86 <roddg@mail.ru>
2022-08-20 11:54:49 +0900

    Creating a fragment

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CrimeFragment : Fragment() {
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        return view
+    }
+}
