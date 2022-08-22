77d5a269480775fb6fbab11b4ca135d0fe403d3a
roddg86 <roddg@mail.ru>
2022-08-22 19:47:47 +0900

    Configuring Repository Properties

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -1,9 +1,21 @@
 package com.bignerdranch.android.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import com.bignerdranch.android.criminalintent.database.CrimeDatabase
+
+private const val DATABASE_NAME = "crime-database"
 
 /* синглтон */
 class CrimeRepository private constructor(context: Context) {
+    private val database:CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val crimeDao = database.crimeDao()
+
     companion object {
         private var INSTANCE: CrimeRepository? = null
         fun initialize(context: Context) {
