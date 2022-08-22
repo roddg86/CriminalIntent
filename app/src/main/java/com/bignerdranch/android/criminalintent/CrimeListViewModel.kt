4d9fe19828c1e7debb00c745499b43fa1e555bdf
roddg86 <roddg@mail.ru>
2022-08-22 20:21:09 +0900

    Returning LiveData to the DAO

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
+    /* Доступ к репозиторию в ViewModel */
     private val crimeRepository = CrimeRepository.get()
     val crimes = crimeRepository.getCrimes()
 }
