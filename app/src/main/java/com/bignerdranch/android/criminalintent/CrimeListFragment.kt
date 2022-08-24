83e42c1c61884a6c3a8026dfe55e722fa71a76be
roddg86 <roddg@mail.ru>
2022-08-24 18:51:08 +0900

    Menu add

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -3,9 +3,7 @@ package com.bignerdranch.android.criminalintent
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -28,13 +26,18 @@ class CrimeListFragment : Fragment() {
     private var adapter: CrimeAdapter? = CrimeAdapter()
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this).get(CrimeListViewModel::class.java)
-
     }
 
     override fun onAttach(context: Context) {
         Log.i(TAG, "onAttach")
         super.onAttach(context)
         callbacks = context as Callbacks?
+    }
+
+    /* Получение обратных вызовов */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -120,7 +123,6 @@ class CrimeListFragment : Fragment() {
         }
     }
 
-
     companion object {
         fun newInstance(): CrimeListFragment {
             return CrimeListFragment()
@@ -130,5 +132,23 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    /* Заполнение ресурса меню */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -6,4 +6,9 @@ class CrimeListViewModel : ViewModel() {
     /* Доступ к репозиторию в ViewModel */
     private val crimeRepository = CrimeRepository.get()
     val crimeListLiveData = crimeRepository.getCrimes()
+
+    /* Реакция на выбор команд */
+    fun addCrime(crime: Crime){
+        crimeRepository.addCrime(crime)
+    }
 }
