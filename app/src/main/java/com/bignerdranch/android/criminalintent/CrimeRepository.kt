f803f55b52e911523e071cb39815c79aa7a01918
roddg86 <roddg@mail.ru>
2022-08-25 13:41:50 +0900

    Choosing a photo location

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.bignerdranch.android.criminalintent.database.CrimeDatabase
 import com.bignerdranch.android.criminalintent.database.migration_1_2
+import java.io.File
 import java.util.*
 import java.util.concurrent.Executors
 
@@ -19,6 +20,7 @@ class CrimeRepository private constructor(context: Context) {
     ).addMigrations(migration_1_2).build() // настройка миграции
     private val crimeDao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor()
+    private val filesDir = context.applicationContext.filesDir
 
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
 
@@ -35,6 +37,8 @@ class CrimeRepository private constructor(context: Context) {
             crimeDao.addCrime(crime)
         }
     }
+
+    fun getPhotoFile(crime: Crime): File = File(filesDir, crime.photoFileName)
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
